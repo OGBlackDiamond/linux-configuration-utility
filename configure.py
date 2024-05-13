e9e5f0497e7a673b7f456d6c2f80c9364abe1cdb
OGBlackDiamond <caden.feller@gmail.com>
2024-05-12 21:33:47 -0700

    fixed missing code block

--- configure.py
+++ configure.py
@@ -1,14 +1,29 @@
 import argparse
+from os import kill
 import pathlib
-import shutil
+
+parser = argparse.ArgumentParser(
+    prog="Linux Configuration Utility",
+    description="A configuration utility for linux systems to reduce time needed to setup.",
+)
+
+
+parser.add_argument("-n", "--nvim", action="store_true")
 
 
 class Configure:
 
-    home_directory : pathlib.PosixPath
+    home_directory: pathlib.PosixPath
 
+    args: argparse.Namespace
+    
     def __init__(self) -> None:
         self.home_directory = pathlib.PosixPath.home()
+        self.args = parser.parse_args()
+        self.handle_args()
+
+    def handle_args(self):
+        if self.args.nvim:
 
     def get_home_directory(self) -> pathlib.PosixPath:
         return self.home_directory
--- neovim/neovim.py
+++ neovim/neovim.py
@@ -14,8 +14,16 @@ class NeoVim:
 
     def __init__(self, configure: Configure) -> None:
         self.nvim_directory = configure.get_home_directory().joinpath(".config/nvim/")
-
-    def ensure_fs(self):
-        self.nvim_directory.mkdir(parents=True, exist_ok=True)
         self.lua_directory = self.nvim_directory.joinpath("lua/")
         self.plugins_directory = self.lua_directory.joinpath("plugins/")
+
+    def ensure_fs(self) -> None:
+        self.nvim_directory.mkdir(parents=True, exist_ok=True)
+        self.lua_directory.mkdir(parents=True, exist_ok=True)
+        self.plugins_directory.mkdir(parents=True, exist_ok=True)
+
+    def copy_all(self) -> None:
+        shutil.copytree("src/", self.nvim_directory)
+
+    def remove_all(self) -> None:
+        shutil.rmtree(self.nvim_directory)
