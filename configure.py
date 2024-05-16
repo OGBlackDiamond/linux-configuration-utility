bff73b32bfe1b121a239f1b25f0ec4f3a4d8aebc
Caden Feller <caden.feller@gmail.com>
2024-05-16 14:05:05 -0700

    started adding more command-line arguments

--- configure.py
+++ configure.py
@@ -1,13 +1,14 @@
 import argparse
-from os import kill
 import pathlib
+
+from neovim.neovim import NeoVim
 
 parser = argparse.ArgumentParser(
     prog="Linux Configuration Utility",
     description="A configuration utility for linux systems to reduce time needed to setup.",
 )
 
-
+parser.add_argument("-r", "--remove", action="store_true")
 parser.add_argument("-n", "--nvim", action="store_true")
 
 
@@ -16,14 +17,18 @@ class Configure:
     home_directory: pathlib.PosixPath
 
     args: argparse.Namespace
-    
+
     def __init__(self) -> None:
         self.home_directory = pathlib.PosixPath.home()
         self.args = parser.parse_args()
         self.handle_args()
 
     def handle_args(self):
-        if self.args.nvim:
-            pass
+        if self.args.nvim and self.args.remove:
+            print("Hello")
+
     def get_home_directory(self) -> pathlib.PosixPath:
         return self.home_directory
+
+
+a = Configure()
--- neovim/neovim.py
+++ neovim/neovim.py
@@ -1,8 +1,5 @@
-import os
 import pathlib
 import shutil
-
-from configure import Configure
 
 
 class NeoVim:
@@ -12,7 +9,7 @@ class NeoVim:
     lua_directory: pathlib.PosixPath
     plugins_directory: pathlib.PosixPath
 
-    def __init__(self, configure: Configure) -> None:
+    def __init__(self, configure) -> None:
         self.nvim_directory = configure.get_home_directory().joinpath(".config/nvim/")
         self.lua_directory = self.nvim_directory.joinpath("lua/")
         self.plugins_directory = self.lua_directory.joinpath("plugins/")
@@ -23,7 +20,7 @@ class NeoVim:
         self.plugins_directory.mkdir(parents=True, exist_ok=True)
 
     def copy_all(self) -> None:
-        shutil.copytree("src/", self.nvim_directory)
+        shutil.copytree("src/", self.nvim_directory, dirs_exist_ok=False)
 
     def remove_all(self) -> None:
         shutil.rmtree(self.nvim_directory)
